aa83efb776d1b8db372bc5c8d23cee0be64eb824
Thirumal <m.thirumal@hotmail.com>
2019-12-24 21:31:55 +0530

    updated

---
+++ src/main/java/in/thirumal/ErmPostgresqlSpringBootApplication.java
@@ -0,0 +1,13 @@
+package in.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmPostgresqlSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErmPostgresqlSpringBootApplication.class, args);
+	}
+
+}
